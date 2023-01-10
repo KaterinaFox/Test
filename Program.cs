e0fc9fcf3db98ab9ba13ec6452c941c6b0aa0d52
Ekaterina Lisichkina <Katyly@yandex.ru>
2023-01-10 14:32:18 +0300

    changes Program.cs

--- Program.cs
+++ Program.cs
@@ -1,45 +1,45 @@
-﻿void PrintArray (string [] array)
+﻿void PrintArray(string[] array)
 {
-Console.Write("Введенный массив ");
-Console.Write("[");
-for (int i = 0; i < array.Length; i++)
-{
-if (i < array.Length - 1) Console.Write($"'{array[i]}', ");
-else Console.Write($"'{array[i]}'"); 
-}
-Console.WriteLine("]");
+    Console.Write("Введенный массив ");
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"\"{array[i]}\", ");
+        else Console.Write($"\"{array[i]}\"");
+    }
+    Console.WriteLine("]");
 }
 
-void PrintArrayElements (string [] array)
+void PrintArrayElements(string[] array)
 {
-Console.Write("Элементы массива, у которых 3 и меньше символов: ");
-Console.Write("[");
-for (int i = 0; i < array.Length; i++)
-{
-if (array[i].Length <= 3) 
-Console.Write($" '{array[i]}' ");
-//else Console.Write($"{array[i]}");
+    Console.Write("Элементы массива, у которых 3 и меньше символов: ");
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+            Console.Write($" \"{array[i]}\" ");
+        //else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
 }
-Console.WriteLine("]");
-}
-    
 
 
-Console.WriteLine ("Введите размер одномерного строчного массива");
+Console.Clear();
+Console.WriteLine("Введите размер одномерного строчного массива");
 int length = Convert.ToInt32(Console.ReadLine());
-if (length >= 0)
+if (length > 0)
 {
-string [] arr = new string [length];
-for (int i = 0; i < length; i++)
-{
-Console.WriteLine( $"Введите {i+1} элемент массива");
-arr [i] = Console.ReadLine();    
-}
-PrintArray (arr);
-PrintArrayElements (arr);
+    string[] arr = new string[length];
+    for (int i = 0; i < length; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} элемент массива");
+        arr[i] = Console.ReadLine();
+    }
+    PrintArray(arr);
+    PrintArrayElements(arr);
 }
 
-else Console.WriteLine ("Недопустимое значение размерности массива");
+else Console.WriteLine("Введено недопустимое значение размерности массива");
 
 
 
